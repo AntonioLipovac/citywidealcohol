d0c6915c2691a93dca34f0f6e5b65b5616bb762b
AntonioLipovac <75137131+AntonioLipovac@users.noreply.github.com>
2022-09-14 14:22:57 +0200

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,19 @@
 <template>
-  <div class="home">
-  
-    <HelloWorld msg=""/>
-   
+  <div class="row">
+  <div class="col-1"></div>
+  <div class="col-8">
+    <div class="card">
+  <h5 class="card-header">Featured</h5>
+  <div class="card-body p-0">
+    <img class="card-img-top" src="" 
   </div>
-</template>
+</div>
+  </div>
+  <div class="col-2">
+  ... ovdje ide desni stupac!
+  </div>
+  </div>
+ </template>
 
 <script>
 // @ is an alias to /src
